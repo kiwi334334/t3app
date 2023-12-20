ccce80d160838fe03cfc5570a2f0fadeab8a204a
Elias Willey <willey.elias@explore.academy>
2023-12-20 07:25:57 -0700

    modified:   src/app/about/page.tsx 	modified:   src/app/page.tsx 	modified:   src/styles/globals.css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,41 @@
+"use client"
+import { useState } from "react";
 import NavBar from "~/components/navbar";
 
+interface signinproptypes {
+  bool: boolean,
+  username: string,
+  changeusername:any
+  togglesigninbool:any
+};
+
 export default function Aboutpage() {
+  const [signedinbool, togglesigninbool] = useState(false);
+  const [username, changeusername] = useState("");
+  return (
+    <div className="w-screen h-screen bg-blue-100 animate-gradient-x bg-gradient-to-r from-green-400 via-emerald-400 to-cyan-500">
+      <NavBar />
+      <div className="">
+        <Signin bool={signedinbool}  username={username} changeusername={changeusername} togglesigninbool={togglesigninbool} />
+      </div>
+    </div>
+  );
+};
+
+function Signin({ bool, username, changeusername, togglesigninbool }: signinproptypes) {
+  if (bool) {
     return (
-        <div className="w-screen h-screen bg-blue-100 animate-gradient-x bg-gradient-to-r from-green-400 via-emerald-400 to-cyan-500">
-            <NavBar />
-            <div className="flex justify-center align-middle">
-                <h1>Hello</h1>
-            </div>
-        </div>
-    )
-}
+      <>
+        <h1>hello{username}</h1>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Please Login </h1><br/>
+        <input onChange={(e) => {changeusername(e.target.value)}} placeholder="Username" />
+        <button onClick={() => {togglesigninbool(true)}}>Sign In</button>
+      </>
+    );
+  };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import { useBetween } from "use-between";
 import NavBar from "~/components/navbar";
 
+interface itemtypes {
+  imgURL: string,
+  price: number,
+  title: string,
+  type: string
+};
+
 const useFormState = () => {
   const [cart, addtocart] = useState(["THIS IS THE CART", "WHEN YOU ADD IT WILL SHOW HERE"])
   const [cost, addtocost] = useState(0)
@@ -20,7 +27,7 @@ const useFormState = () => {
 const useSharedFormState = () => useBetween(useFormState);
 
 export default function HomePage() {
-  const { cart, cost, allamounts } = useSharedFormState();
+  const { cart, cost, allamounts, addtoallamounts, addtocart, addtocost } = useSharedFormState();
   return (
     <div className="w-screen h-screen">
       <NavBar />
@@ -34,7 +41,7 @@ export default function HomePage() {
       <div className="fixed right-[30%] h-full w-[40%] overflow-scroll">
         <div className="overflow-scroll h-full w-full bg-stone-300 scrollbar-hide p-10">
           <h1>COUCHES</h1>
-          <Iteminstore type={"Couch"} title={"NIKEA Auto Fold"} price={400} imgURL={"https://d2xcq4qphg1ge9.cloudfront.net/assets/130047/3795631/original_sofa_1000px.png"} /><br />
+          <Iteminstore type={"Couch"} title={"NIKEA Auto Fold 300"} price={400} imgURL={"https://static.vecteezy.com/system/resources/previews/022/219/389/original/white-sofa-isolated-on-a-transparent-background-png.png"} /><br />
           <Iteminstore type={"Couch"} title={"NIKEA Auto Fold"} price={300} imgURL={"/couch2.png"} /><br />
           <h1>TABLES</h1>
           <Iteminstore type={"Table"} title={"NIKEA Basic Table"} price={250} imgURL={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Dinner_table_side_pine_wood_large_table.png/1200px-Dinner_table_side_pine_wood_large_table.png"} /><br />
@@ -48,13 +55,16 @@ export default function HomePage() {
             <h1 key={i}>— {item}:{allamounts[i]}</h1>
           );
         })}
-        <h1 className="bottom-0 absolute ">the final price is {cost}</h1>
+        <div className="bottom-0 absolute ">
+          <h1>the final price is {cost}</h1>
+          <button onClick={() => {addtoallamounts([]); addtocart([]); addtocost(0)}}>Clear Cart</button>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-function Iteminstore({ imgURL, price, title, type }) {
+function Iteminstore({ imgURL, price, title, type }: itemtypes) {
   const { cart, cost, addtocost, addtocart, allamounts, addtoallamounts } = useSharedFormState();
   const [amount, setamount] = useState(1);
   const copyallamounts = allamounts
@@ -73,5 +83,5 @@ function Iteminstore({ imgURL, price, title, type }) {
         <button className="border-3 border-solid border-black" id="1btn" onClick={() => { addtocart([...cartcopy, title]); addtocost(cost + (price * amount)); addtoallamounts([...copyallamounts, `${amount}`]) }}>{title}</button>
       </div>
     </>
-  )
-}
+  );
+};
